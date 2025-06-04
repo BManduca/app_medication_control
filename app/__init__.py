b07d0498e6fb56958ce3d8b70f55125e2bb71203
Brunno Manduca <brunnomanducarfe@gmail.com>
2025-06-03 21:42:25 -0300

    refactor: trocando método Query.get() do SQLAlchemy que ficará obsoleto. Passando a utilizar db.session.get()

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timezone
 import os
 from dotenv import load_dotenv
 from flask import Flask
@@ -13,11 +14,12 @@ db = SQLAlchemy()
 migrate = Migrate()
 login_manager = LoginManager()
 login_manager.login_view = 'auth.login' # Redirecionamento se não estiver logado
-login_manager.login_message_category = 'info'
+login_manager.login_message = 'Você precisa estar logado para acessar esta página!'
+login_manager.login_message_category = 'warning' # categoria do flash
 
 # função é responsável por criar e configurar a aplicação Flask, com banco de dados, 
 # autenticação, rotas e configurações gerais
-def create_app():
+def create_app(testing=False):
     app = Flask(__name__)
 
     # carregando configurações do arquivo config.py | define se é dev ou prod
@@ -26,6 +28,14 @@ def create_app():
         app.config.from_object(ProdConfig)
     else:
         app.config.from_object(DevConfig)
+
+    # se testing=True, sobrescreve as configs para testes
+    if testing:
+        app.config.update({
+            "TESTING": True,
+            "SQLALCHEMY_DATABASE_URI": "sqlite:///:memory:",
+            "WTF_CSRF_ENABLED": False,  # Desativa CSRF para testes
+        })
 
     # Inicializa extensões com a aplicação
     db.init_app(app)
@@ -37,11 +47,17 @@ def create_app():
 
     from app.routes.auth_routes import auth_bp
     from app.routes.dashboard_routes import dashboard_bp
-    from app.main_routes import main
+    from app.routes.medication_routes import medication_bp
+    from app.routes.main_routes import main
 
     app.register_blueprint(auth_bp) # Rotas de autenticação
     app.register_blueprint(dashboard_bp) # Rota protegia: /dashboard
+    app.register_blueprint(medication_bp) # Rotas dos medicamentos
     app.register_blueprint(main) # Rota principal: /
+
+    @app.context_processor
+    def inject_now():
+        return {'now': datetime.now(timezone.utc)}
 
     return app
 
@@ -49,4 +65,4 @@ from app.models import User
 
 @login_manager.user_loader
 def load_user(user_id):
-    return User.query.get(int(user_id))
+    return db.session.get(User, int(user_id))
