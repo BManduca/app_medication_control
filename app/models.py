54651648b51d454d014368973cbd19c6395880a0
Brunno Manduca <brunnomanducarfe@gmail.com>
2025-06-16 21:15:28 -0300

    Alterando configuração models para que password_hash possa ser Nullable

--- app/models.py
+++ app/models.py
@@ -13,7 +13,7 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.Text, nullable=False) # campo para senha tradicional
+    password_hash = db.Column(db.Text, nullable=True)  # agora permite NULL para usuários Google
     is_google_user = db.Column(db.Boolean, default=False) # <------- Flag
 
     registers = db.relationship('Register', backref='user', lazy=True)
