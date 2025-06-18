72646f3047caa1c91eb98371cd2ab4c16ccd1cf7
Brunno Manduca <brunnomanducarfe@gmail.com>
2025-06-17 21:03:20 -0300

    chore: atualizando configurações do projeto e tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/templates/**/*.html"    
+    "./app/templates/**/*.html", // inclui todos os arquivos html em subpastas
+    "./app/templates/**/**/*.html" // segurança extra para subpastas mais profundas
   ],
   darkMode: 'class',
   theme: {
